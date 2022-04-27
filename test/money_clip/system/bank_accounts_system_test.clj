9c8f03c1c58f722f4fdcd6b8cd2872be0b41a793
Miguel Guinada <mguinada@gmail.com>
2022-04-27 20:51:42 +0100

    Fix bank account `bank-name` submission

--- test/money_clip/system/bank_accounts_system_test.clj
+++ test/money_clip/system/bank_accounts_system_test.clj
@@ -12,9 +12,9 @@
 (deftest post-bank-account-test
   (let [[_ auth-token] (system/create-user-and-login @system/app "test.user@users.com")]
     (testing "when the bank-account name is not taken"
-      (let [response (POST @system/app "/bank-accounts" {:name "Savings"} :headers {"Authorization" (str "Token " auth-token)})]
+      (let [response (POST @system/app "/bank-accounts" {:name "Savings" :bank-name "IBANK"} :headers {"Authorization" (str "Token " auth-token)})]
         (is (= 201 (http/status response)) "Creates the bank account")
-        (is (= :bank-account (-> (http/body response) keys first)) "Returns the payload")))
+        (is (= {:name "Savings" :bank-name "IBANK"} (-> (http/body response) :bank-account (select-keys [:name :bank-name]))) "Returns the payload")))
     (testing "when the bank-account name is taken"
       (let [response (POST @system/app "/bank-accounts" {:name "Savings"} :headers {"Authorization" (str "Token " auth-token)})]
         (is (= 412 (http/status response)) "Serves an HTTP status code")
