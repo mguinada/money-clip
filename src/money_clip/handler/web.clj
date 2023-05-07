5f27eb4c7f9603060a9e5c1f13695276eff3debd
mguinada <mguinada@gmail.com>
2023-05-07 15:57:58 +0100

    Add a landing zone

---
+++ src/money_clip/handler/web.clj
@@ -0,0 +1,8 @@
+(ns money-clip.handler.web
+  (:require [ataraxy.response :as response]
+            [clojure.java.io :as io]
+            [integrant.core :as ig]))
+
+(defmethod ig/init-key :money-clip.handler.web/index [_ _]
+  (fn [_]
+    [::response/ok (io/resource "money_clip/html/index.html")]))
