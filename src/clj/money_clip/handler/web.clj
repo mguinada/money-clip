a05f33da7603271ef50a91111839c03f7f169bef
mguinada <mguinada@gmail.com>
2023-05-12 17:33:46 +0100

    Add site middleware

--- src/clj/money_clip/handler/web.clj
+++ src/clj/money_clip/handler/web.clj
@@ -1,8 +1,8 @@
 (ns money-clip.handler.web
-  (:require [ataraxy.response :as response]
-            [clojure.java.io :as io]
-            [integrant.core :as ig]))
+  (:require  [compojure.core :refer [GET]]
+             [clojure.java.io :as io]
+             [integrant.core :as ig]))
 
 (defmethod ig/init-key :money-clip.handler.web/index [_ _]
-  (fn [_]
-    [::response/ok (io/resource "money_clip/public/index.html")]))
+  (GET "/" []
+    (io/resource "money_clip/public/index.html")))
