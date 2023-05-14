8ddf10595c72cd77e2b2bb5a72398da19402c0be
mguinada <mguinada@gmail.com>
2023-05-14 11:51:40 +0100

    Add route for frontend navigation

--- src/clj/money_clip/handler/web.clj
+++ src/clj/money_clip/handler/web.clj
@@ -3,6 +3,10 @@
              [clojure.java.io :as io]
              [integrant.core :as ig]))
 
-(defmethod ig/init-key :money-clip.handler.web/index [_ _]
+(defmethod ig/init-key :money-clip.handler.web/root [_ _]
   (GET "/" []
+    (io/resource "money_clip/public/index.html")))
+
+(defmethod ig/init-key :money-clip.handler.web/site [_ _]
+  (GET "/:path" [_]
     (io/resource "money_clip/public/index.html")))
